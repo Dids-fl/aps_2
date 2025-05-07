74b5ed7dfba69e46478ee1a3ae81fcd626a1db34
Dids-fl <diegofla@al.insper.edu.br>
2025-05-06 21:41:03 -0300

    Q2

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,10 @@
 import random
 def rolar_dados(quantidade):
     return [random.randint(1, 6) for _ in range(quantidade)]
+
+def guardar_dado(dados_rolados,dados_no_estoque,dado_para_guardar):
+    dados_no_estoque.append(dados_rolados[dado_para_guardar])
+    del dados_rolados[dado_para_guardar]
+    lista = [dados_rolados,dados_no_estoque]
+    return lista
+
